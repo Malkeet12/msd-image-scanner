b4be287002a2cc8b0044ea55c0b8402e00126efa
Malkeet singh <bs001@MacBook-Pro.domain.name>
2020-07-11 19:07:07 +0530

    double tap to zoom out

--- lib/shared_widgets/zoom.dart
+++ lib/shared_widgets/zoom.dart
@@ -11,20 +11,23 @@ class ZoomableWidget extends StatefulWidget {
 
 class _ZoomableWidgetState extends State<ZoomableWidget> {
   Matrix4 matrix = Matrix4.identity();
-  Matrix4 zerada = Matrix4.identity();
+  Matrix4 orginal = Matrix4.identity();
+  bool zoomedIn = false;
+  // Matrix4 myMatrix = Matrix4.identity()..getMaxScaleOnAxis();
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onDoubleTap: () {
         setState(() {
-          matrix = zerada;
+          matrix = orginal;
+          zoomedIn = !zoomedIn;
         });
       },
       child: MatrixGestureDetector(
         shouldRotate: false,
-        shouldScale: true,
-        shouldTranslate: false,
+        // shouldScale: true,
+        // shouldTranslate: false,
         onMatrixUpdate: (Matrix4 m, Matrix4 tm, Matrix4 sm, Matrix4 rm) {
           setState(() {
             matrix = m;
@@ -32,7 +35,7 @@ class _ZoomableWidgetState extends State<ZoomableWidget> {
         },
         child: Transform(
           transform: matrix,
-          child: widget.child,
+          child: Center(child: widget.child),
         ),
       ),
     );
